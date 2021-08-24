aa437d5d78b5c01aae3217ef4a7902da9ddf1569
sebastian98Gonzalez <sebastiangonzalez98sebastian@gmail.com>
2021-08-23 23:12:27 -0500

    paso1 i2

--- facebook.js
+++ facebook.js
@@ -2,6 +2,7 @@ console.log("asgasgh")
 let button=document.getElementById("button");
 button.addEventListener("click",()=>{
     FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
+        // statusChangeCallback(response);
+        console.log(response)
     });
 })
